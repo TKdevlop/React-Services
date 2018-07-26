0cc199cb396f534add39f7ef35f05b2e7c0dfa62
TKdevlop <34345518+TKdevlop@users.noreply.github.com>
2018-07-27 01:38:53 +0530

    Final commit

--- src/store/actions/firebase.js
+++ src/store/actions/firebase.js
@@ -1,12 +1,7 @@
 import firebase from 'firebase/app';
 import 'firebase/auth';
 const config = {
-  apiKey: "AIzaSyDI-BRZ_hyXcyw_gRfJAr-JlIKIZpwX6sE",
-  authDomain: "food-ordering-6ccba.firebaseapp.com",
-  databaseURL: "https://food-ordering-6ccba.firebaseio.com",
-  projectId: "food-ordering-6ccba",
-  storageBucket: "food-ordering-6ccba.appspot.com",
-  messagingSenderId: "298314384548"
+//your firebase credentials
 };
 firebase.initializeApp(config);
 
